b58e3c292d03d26ed74f58daddff020cba43bc2c
Ousmane DIOP <49846215+Nooreyni@users.noreply.github.com>
2025-02-02 16:16:11 +0100

    Add files via upload

---
+++ gatsby-cofig.js
@@ -0,0 +1,12 @@
+module.exports = {
+  siteMetadata: {
+    title: "Nooreyni - Digital Solutions",
+    description: "Portfolio d'Usman, expert en IT et IA",
+  },
+  plugins: [
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-netlify",
+    "gatsby-plugin-image",
+    "gatsby-transformer-sharp",
+  ],
+};
